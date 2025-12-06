25d4b6e0deda81c2c0c6af188dce70571191461c
Moncef Yakoubi <mono.yakoubi@gmail.com>
2025-12-06 16:46:28 +0100

    Enhance flag submission with validation and API call
    Added validation and API submission for flag input.

--- src/Pages/pupup/bimo/bimo.jsx
+++ src/Pages/pupup/bimo/bimo.jsx
@@ -14,10 +14,49 @@ const Bimo = () => {
     setIsPopupOpenbimo(false);
   };
 
-  const handleSubmitFlagbimo = (flag) => {
-    console.log('Flag submitted:', flag);
-    setIsPopupOpenbimo(false);
-    alert(`Flag submitted: ${flag}`);
+  const handleSubmitFlagbimo = async (flag) => {
+
+    flag = flag.trim()
+    console.log("flag:", flag);
+    console.log("Flag length:", flag.length);
+    if(flag.length > 128){
+      setError("Flag must contain 128 characters or less")
+      return;
+    }
+
+    if(!flag){
+      setError("Flag must not be empty")
+      return;
+    }
+
+    try {
+      const res = await api.post("/game/flags/submit/",
+        {
+          flag_code: flag,
+        },
+        {
+          headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          }
+        }
+      )
+
+      if(res.data.success){
+        navigate("/")
+        console.log('Flag submitted:', flag);
+        alert(`Success! ${res.data.points_earned} points earned at ${res.data.stand}`);
+      }else{
+        setError(res.data.message)
+        console.log("submittion failed: ", res.data.message)
+      }
+    } catch (error) {
+      setError(error.response?.data?.message || "something went wrong")
+      console.error("Submission failed:", error);
+      console.log("Error response:", error.response.data);
+    }
+
+    setIsPopupOpenbimo(false); 
   };
   
   return (
